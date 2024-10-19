9ce4a835425366fa09b979ec3249437c2b1addbc
miguelDT <mtamargo122@gmail.com>
2024-10-19 17:19:55 -0400

    clean and refactor

--- Project 3/p3.c
+++ Project 3/p3.c
@@ -10,63 +10,121 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int MAX = 2000000;
+int MAX_UPDATES = 2000000;
+/* Mutex lock */
+pthread_mutex_t mutex;
 
-struct counts
+/* Struct a shared variable to store result */
+struct shared_data
 {
-    int counter;
-    int bonus_counter;
+    int value;
+    int bonus;
+    int thread1_updates;
+    int thread2_updates;
 };
+/* Global shared variable */
+struct shared_data *counter;
 
-
-void *count_up(void* arg){
-    struct counts *data = (struct counts *)arg;
-    int i = 0;
-
-        while (i < MAX)
+/* Thread1 function */
+void *thread1_funct()
+{
+    while (counter->thread1_updates < MAX_UPDATES)
     {
-        if (data->counter % 100 == 0)
+        if (pthread_mutex_trylock(&mutex) == 0)
         {
-            data->counter += 100;
-            data->bonus_counter += 1;
-            i+=100;
-        } else{
-            data->counter +=1;
-            i+=1;
+            if ((counter->value % 100) == 0 && counter->thread1_updates + 100 <= MAX_UPDATES)
+            {
+                counter->value += 100;
+                counter->bonus++;
+                counter->thread1_updates += 100;
+            }
+            else
+            {
+                counter->value ++;
+                counter->thread1_updates++;
+            }
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+    
+    printf("Im thread 1, I did %d updates and got the bonus %d times, counter = %d\n", counter->thread1_updates, counter->bonus, counter->value);
+    return NULL;
+}
+
+/* Thread2 function */
+void *thread2_funct()
+{
+    while (counter->thread2_updates < MAX_UPDATES)
+    {
+        if (pthread_mutex_trylock(&mutex) == 0)
+        {
+            counter->value++;
+            counter->thread2_updates++;
+            pthread_mutex_unlock(&mutex);
         }
     }
 
+    printf("Im thread 2, I did %d updates, counter = %d\n", counter->thread2_updates, counter->value);
     return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
-    pthread_t tid[2];
-    int rc;
-    
-    struct counts counters;
-    counters.counter = 0;
-    counters.bonus_counter = 0;
+    /* Allocate memory for shared data */
+    counter = (struct shared_data *) malloc(sizeof(struct shared_data));
+    counter->value = 0;
+    counter->bonus = 0;
+    counter->thread1_updates = 0;
+    counter->thread2_updates = 0;
 
-    pthread_t thread;
+    pthread_t threads[2];
     int err;
 
-    err = pthread_create(&thread, NULL, count_up, (void *)&counters);
-    if (err) {
-        printf("Error creating thread\n");
-        return 1;
+    /* Initialize mutex lock */
+    if ((pthread_mutex_init(&mutex, NULL))) {
+        printf("Error occured when initialize mutex lock.");
+        exit(0);
     }
 
-    printf("Starting Thread: %lu\n\n", (unsigned long)thread);
-    printf("Waiting for the thread to end...\n\n");
+    pthread_attr_t attr;
+    if ((pthread_attr_init(&attr))) {
+        printf("Error occured when initialize pthread_attr_t.");
+        exit(0);
+    }
 
-    pthread_join(thread, NULL);
+    /* Create thread1 */
+    if ((err = pthread_create(&threads[0], NULL, &thread1_funct, NULL)))
+    {
+        fprintf(stderr, "ERROR: pthread_create, Code: %d\n", err);
+        exit(0);
+    }
 
-    printf("Ending Thread: %lu\n", (unsigned long)thread);
-    printf("Counter Finished at: %d\nThe bonus counter was %d\n", counters.counter, counters.bonus_counter);
+    /* Create thread2 */
+    if ((err = pthread_create(&threads[1], NULL, &thread2_funct, NULL)))
+    {
+        fprintf(stderr, "ERROR: pthread_create, Code: %d\n", err);
+        exit(0);
+    }
+
+    /* Wait for threads to finish */
+    for (int i = 0; i < 2; i++)
+    {
+        if (pthread_join(threads[i], NULL) != 0)
+        {
+            return 2;
+        }
+    }
+
+    /* Clean up */
+    pthread_mutex_destroy(&mutex);
+    free(counter);
+    pthread_exit(NULL);
+
+    printf("From parent: counter = %d\n", counter->value);
 
     return 0;
 }
+
 //Task 1:
 // create 2 threads
 // share data structure called counter; each will increment toi a total of 2 mil
